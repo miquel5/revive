e44161bd1c4725974b91b77b9df2bba994d51aad
Miquel <llauradofilella@gmail.com>
2025-02-06 17:58:55 +0100

    Add footer

--- src/app/components/crud/crud.component.ts
+++ src/app/components/crud/crud.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FiltersComponent } from "../filters/filters.component";
 import { OverviewComponent } from "../overview/overview.component";
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-crud',
@@ -10,8 +11,35 @@ import { OverviewComponent } from "../overview/overview.component";
   styleUrl: './crud.component.css'
 })
 
-export class CrudComponent
+export class CrudComponent implements OnInit
 {
-  title: string = 'Crud';
-  description: string = 'Esta es la descripción del crud';
+    title: string = ' ';
+    description: string = ' ';
+  
+    private crudConfigs = [
+        ['/users', 'Gestión de usuarios', 'Administra los usuarios del sistema'],
+        ['/history','Historial','Historial de objetos'],
+        ['/warehouse', 'Gestión de almacén', 'Administra el inventario del almacén'],
+        ['/organizations', 'Gestión de organizaciones', 'Administra las organizaciones registradas'],
+        ['/categories', 'Gestión de categorías', 'Administra las categorías del sistema']
+    ];
+  
+    constructor(private router: Router) {}
+  
+    ngOnInit()
+    {
+        this.setTitleAndDescription();
+    }
+  
+    private setTitleAndDescription()
+    {
+        const currentRoute = this.router.url;
+        const config = this.crudConfigs.find(([path]) => path === currentRoute);
+      
+        if (config)
+        {
+            [, this.title, this.description] = config;
+        }
+    }
 }
+  
